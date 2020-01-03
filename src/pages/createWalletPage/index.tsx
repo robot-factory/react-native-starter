f7ca126438802f8d77541799389bc1c7122a683e
liuboheng <lbh9311@sina.com>
2020-01-03 11:42:48 +0800

    增加了KeyboardAwareScrollView，用于解决TextInput的自动去焦

--- src/pages/createWalletPage/index.tsx
+++ src/pages/createWalletPage/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import {
   View,
   Text,
@@ -6,30 +6,31 @@ import {
   NativeSyntheticEvent,
   TextInputEndEditingEventData,
   TouchableWithoutFeedback,
-} from 'react-native'
-import {NavigationStackScreenProps} from 'react-navigation-stack'
+} from 'react-native';
+import {NavigationStackScreenProps} from 'react-navigation-stack';
+import {KeyboardAwareScrollView} from 'react-native-keyboard-aware-scroll-view';
 
 class CreateWalletPage extends React.Component<NavigationStackScreenProps> {
   private mnemonicInput: TextInput | undefined
   constructor(props: NavigationStackScreenProps) {
-    super(props)
-    this.state = {text: ''}
+    super(props);
+    this.state = {text: ''};
   }
   endEditing(param: string) {
-    console.log(param)
+    console.log(param);
   }
 
   submiteContext(param: string) {
-    console.log(param)
+    console.log(param);
   }
 
   private mountMnemonicInput = (instance: TextInput) => {
-    this.mnemonicInput = instance
+    this.mnemonicInput = instance;
   }
 
   private onPress() {
     if (this.mnemonicInput) {
-      this.mnemonicInput.blur()
+      this.mnemonicInput.blur();
     }
   }
 
@@ -39,7 +40,7 @@ class CreateWalletPage extends React.Component<NavigationStackScreenProps> {
     this.endEditing(
       // 退出键盘
       'onEndEditing text: ' + event.nativeEvent.text,
-    )
+    );
   }
 
   handleSubmitEnterMnemonic = (
@@ -48,7 +49,7 @@ class CreateWalletPage extends React.Component<NavigationStackScreenProps> {
     this.submiteContext(
       // 数据提交
       'onSubmitEditing text: ' + event.nativeEvent.text,
-    )
+    );
   }
 
   handelOnChangeText = (text: string) => this.setState({text})
@@ -56,39 +57,44 @@ class CreateWalletPage extends React.Component<NavigationStackScreenProps> {
   render() {
     return (
       <View>
-        <TouchableWithoutFeedback
-          onPress={this.onPress}
-          style={{backgroundColor: 'blue', width: '100%', height: 400}}
-          >
-          <Text>enter your mnemonic</Text>
-          <TextInput
-            ref={this.mountMnemonicInput}
-            style={{
-              height: 300,
-              borderStyle: 'solid',
-              borderColor: '#000',
-              borderWidth: 1,
-              marginTop: 100,
-              marginLeft: 30,
-              marginRight: 30,
-            }}
-            placeholder="输入内容"
-            keyboardType={'default'} // 默认键盘类型
-            multiline={true}
-            autoCapitalize="none" // 取消首字母大写
-            keyboardAppearance={'light'} // 键盘演示
-            returnKeyType={'done'} // 代替 returnKeyType
-            secureTextEntry={false} // 输入内容显示黑色圆点
-            clearButtonMode={'while-editing'} // 输入框右边 x 按钮
-            clearTextOnFocus={false} // 再次输入清空之前内容
-            onChangeText={this.handelOnChangeText} // onChangeText 文本变化是会调用(处于edite状态)
-            onEndEditing={this.handleEndEnterMnemonic}
-            onSubmitEditing={this.handleSubmitEnterMnemonic}
-          />
-        </TouchableWithoutFeedback>
+        <KeyboardAwareScrollView
+          // contentContainerStyle={styles.wrapper}
+          // style={styles.mainWrapper}
+          testID={'first-incoming-transaction-screen'}
+          resetScrollToCoords={{x: 0, y: 0}}>
+          <TouchableWithoutFeedback
+            onPress={this.onPress}
+            style={{backgroundColor: 'blue', width: '100%', height: 400}}>
+            <Text>enter your mnemonic</Text>
+            <TextInput
+              ref={this.mountMnemonicInput}
+              style={{
+                height: 300,
+                borderStyle: 'solid',
+                borderColor: '#000',
+                borderWidth: 1,
+                marginTop: 100,
+                marginLeft: 30,
+                marginRight: 30,
+              }}
+              placeholder="输入内容"
+              keyboardType={'default'} // 默认键盘类型
+              multiline={true}
+              autoCapitalize="none" // 取消首字母大写
+              keyboardAppearance={'light'} // 键盘演示
+              returnKeyType={'done'} // 代替 returnKeyType
+              secureTextEntry={false} // 输入内容显示黑色圆点
+              clearButtonMode={'while-editing'} // 输入框右边 x 按钮
+              clearTextOnFocus={false} // 再次输入清空之前内容
+              onChangeText={this.handelOnChangeText} // onChangeText 文本变化是会调用(处于edite状态)
+              onEndEditing={this.handleEndEnterMnemonic}
+              onSubmitEditing={this.handleSubmitEnterMnemonic}
+            />
+          </TouchableWithoutFeedback>
+        </KeyboardAwareScrollView>
       </View>
-    )
+    );
   }
 }
 
-export default CreateWalletPage
+export default CreateWalletPage;
